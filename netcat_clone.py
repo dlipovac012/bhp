3962372b003fbe83e9a382cf3e80c4a6a162af20
dalibor <dlipovac012@gmail.com>
2020-05-01 14:57:28 -0400

    Usage function done

--- netcat_clone.py
+++ netcat_clone.py
@@ -1,5 +1,6 @@
-#/bin/python3
+#!/bin/python3
 
+import os
 import sys
 import socket
 import getopt
@@ -16,17 +17,35 @@ upload_destination      = ''
 port                    = 0
 
 def usage():
-    print('-=-=-=-= BHP net tool =-=-=-=-')
+    print('########################################################')
+    print('#                                                      #')
+    print('#                    [ BHP net tool ]                  #')
+    print('#                                                      #')
+    print('########################################################')
     print('\n')
-    print('Usage: python3 netcat_clone.py -t <target_ip> -p <port>')
+    print('Usage: ./{} -t <target_ip> -p <port>'.format(__file__))
     print('-l   --listen                    - listen on [host]:[port] for incoming connections')
     print('-e   --execute=<file_to_run>     - execute the given file upon receiving a connection')
     print('-c   --command                   - initialize command shell')
     print('-u   -- upload=<destination>     - upon receiving a connection, upload a file and write to <destination>')
     print('\n\n')
     print('Examples:')
-    print('./netcat_clone.py -t 192.168.0.1 -p 5555 -l -c')
-    print('./netcat_clone.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe')
-    print('./netcat_clone.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\"')
-    print('./netcat_clone.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe')
-    print('echo \'ABCDEFGHI\' | ./bhpnet.py -t 192.168.11.12 -p 135')
+    print('./{} -t 192.168.0.1 -p 5555 -l -c'.format(__file__))
+    print('./{} -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe'.format(__file__))
+    print('./{} -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\"'.format(__file__))
+    print('./{} -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe'.format(__file__))
+    print('echo \'ABCDEFGHI\' | ./{} -t 192.168.11.12 -p 135'.format(__file__))
+    sys.exit(0)
+
+def main():
+    global listen
+    global command
+    global upload
+    global execute
+    global target
+    global upload_destination
+    
+    if not len(sys.argv[1:]):
+        usage()
+
+main()
