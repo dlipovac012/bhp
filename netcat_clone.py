b4976b235932822c797c6531df389ee0e3934876
dalibor <dlipovac012@gmail.com>
2020-05-01 15:05:12 -0400

    small change

--- netcat_clone.py
+++ netcat_clone.py
@@ -17,24 +17,25 @@ upload_destination      = ''
 port                    = 0
 
 def usage():
+    filename = os.path.basename(__file__)
     print('########################################################')
     print('#                                                      #')
     print('#                    [ BHP net tool ]                  #')
     print('#                                                      #')
     print('########################################################')
     print('\n')
-    print('Usage: ./{} -t <target_ip> -p <port>'.format(__file__))
+    print('Usage: ./{} -t <target_ip> -p <port>'.format(filename))
     print('-l   --listen                    - listen on [host]:[port] for incoming connections')
     print('-e   --execute=<file_to_run>     - execute the given file upon receiving a connection')
     print('-c   --command                   - initialize command shell')
     print('-u   -- upload=<destination>     - upon receiving a connection, upload a file and write to <destination>')
     print('\n\n')
     print('Examples:')
-    print('./{} -t 192.168.0.1 -p 5555 -l -c'.format(__file__))
-    print('./{} -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe'.format(__file__))
-    print('./{} -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\"'.format(__file__))
-    print('./{} -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe'.format(__file__))
-    print('echo \'ABCDEFGHI\' | ./{} -t 192.168.11.12 -p 135'.format(__file__))
+    print('./{} -t 192.168.0.1 -p 5555 -l -c'.format(filename))
+    print('./{} -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe'.format(filename))
+    print('./{} -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\"'.format(filename))
+    print('./{} -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe'.format(filename))
+    print('echo \'ABCDEFGHI\' | ./{} -t 192.168.11.12 -p 135'.format(filename))
     sys.exit(0)
 
 def main():
